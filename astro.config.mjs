e8bc005e066c540d1e838eae54d1f576d707dd90
Yiğit Adak <ygtadk100@gmail.com>
2025-11-13 02:59:08 +0300

    Added packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+const isDev = process.env.NODE_ENV === 'development';
+
+export default defineConfig({
+  site: "http://localhost:4321",
+  compressHTML: !isDev,
+  
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
