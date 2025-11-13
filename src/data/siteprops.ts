7c18503772a6c7375529a816c657a52bbc4bd1c9
Yiğit Adak <ygtadk100@gmail.com>
2025-11-13 03:06:53 +0300

    Add basis components and demo pages

---
+++ src/data/siteprops.ts
@@ -0,0 +1,26 @@
+export const siteShortName = 'Limo';
+export const siteName = 'Limo Bilişim ve Teknoloji Hizmetleri';
+export const siteDescription = 'Limo Bilişim ve Teknoloji Hizmetleri resmi web sitesi.';
+export const siteThemes = {
+    default: {
+        themeColor: '#ffffff',
+    }
+}
+export const siteUrl = 'https://limo.com.tr';
+export const siteSocials = {
+    x: {
+        name: 'X (Twitter)',
+        url: 'https://x.com/limobilisim',
+        icon: '/assets/icons/x-icon.png'
+    },
+    linkedin: {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/company/limo-bilişim/',
+        icon: '/assets/icons/linkedin-icon.png'
+    },
+    github: {
+        name: 'GitHub',
+        url: 'https://github.com/limobilisim',
+        icon: '/assets/icons/github-icon.png'
+    }
+};
