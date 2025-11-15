db1b406b42e595bdaf4541299488312571c91010
Yiğit Adak <ygtadk100@gmail.com>
2025-11-15 15:31:05 +0300

    Add Prettier and Preline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig({
-  site: "http://localhost:4321",
-  compressHTML: !isDev,
-  i18n: {
-    locales: ['tr', 'en'],
-    defaultLocale: 'tr',
-    routing: {
-      redirectToDefaultLocale: true,
-      prefixDefaultLocale: true,
+    site: "http://localhost:4321",
+    compressHTML: !isDev,
+    i18n: {
+        locales: ["tr", "en"],
+        defaultLocale: "tr",
+        routing: {
+            redirectToDefaultLocale: false,
+            prefixDefaultLocale: true,
+        },
     },
-  },
-  integrations: [sitemap()],
+    integrations: [sitemap()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
