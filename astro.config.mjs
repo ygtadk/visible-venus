8c209528854ae3cde4b83654f4042b5c74b6de05
Yiğit Adak <ygtadk100@gmail.com>
2025-11-21 19:27:07 +0300

    21.11 19:26

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 const isDev = process.env.NODE_ENV === "development";
@@ -20,7 +20,7 @@ export default defineConfig({
       },
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
 
   vite: {
       plugins: [tailwindcss()],
